77e1d90f8ad8ebf32a12e7df67f9d8c51f6e2a90
abheist <abhishek291093@gmail.com>
2020-03-20 03:20:27 +0530

    Added types and graphy :wink:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./assets/Inter/inter.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hellos</header>
+      <header className="App-header headline2">Hellos</header>
     </div>
   );
 }
