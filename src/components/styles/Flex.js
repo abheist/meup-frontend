71b5d003ede20054bc2d04f8f98692e9db9da9e7
abheist <abhishek291093@gmail.com>
2020-03-25 23:03:34 +0530

    indent Fix

--- src/components/styles/Flex.js
+++ src/components/styles/Flex.js
@@ -3,60 +3,60 @@ import PropTypes from 'prop-types';
 import { COLORS } from './ThemeConstants';
 
 export const FlexItem = styled.div`
-  ${props => props.direction && `flex-direction: ${props.direction};`}
-  ${props => props.justify && `justify-content: ${props.justify};`}
-  ${props => props.content && `align-content: ${props.content};`}
-  ${props => props.align && `align-items: ${props.align};`}
-  ${props => props.wrap && `flex-wrap: ${props.wrap};`}
-  ${props => props.flex && `flex: ${props.flex};`}
-  ${props => props.order && `order: ${props.order};`}
-  ${props => props.self && `align-self: ${props.self};`}
-  ${props =>
+	${props => props.direction && `flex-direction: ${props.direction};`}
+	${props => props.justify && `justify-content: ${props.justify};`}
+	${props => props.content && `align-content: ${props.content};`}
+	${props => props.align && `align-items: ${props.align};`}
+	${props => props.wrap && `flex-wrap: ${props.wrap};`}
+	${props => props.flex && `flex: ${props.flex};`}
+	${props => props.order && `order: ${props.order};`}
+	${props => props.self && `align-self: ${props.self};`}
+	${props =>
 		props.padding &&
 		props.padding.top &&
 		`padding-top: ${props.padding.top + 'px'};`}
-  ${props =>
+	${props =>
 		props.padding &&
 		props.padding.right &&
 		`padding-right: ${props.padding.right + 'px'};`}
-  ${props =>
+	${props =>
 		props.padding &&
 		props.padding.bottom &&
 		`padding-bottom: ${props.padding.bottom + 'px'};`}
-  ${props =>
+	${props =>
 		props.padding &&
 		props.padding.left &&
 		`padding-left: ${props.padding.left + 'px'};`}
 
-  ${props =>
+	${props =>
 		props.margin &&
 		props.margin.top &&
 		`margin-top: ${props.margin.top + 'px'};`}
-  ${props =>
+	${props =>
 		props.margin &&
 		props.margin.right &&
 		`margin-right: ${props.margin.right + 'px'};`}
-  ${props =>
+	${props =>
 		props.margin &&
 		props.margin.bottom &&
 		`margin-bottom: ${props.margin.bottom + 'px'};`}
-  ${props =>
+	${props =>
 		props.margin &&
 		props.margin.left &&
 		`margin-left: ${props.margin.left + 'px'};`}
-  ${props => props.height && `height: ${props.height};`}
-  ${props => props.width && `width: ${props.width};`}
-  ${props => props.minWidth && `min-width: ${props.minWidth + 'px'};`}
-  ${props => props.maxWidth && `max-width: ${props.maxWidth + 'px'};`}
-  ${props => props.background && `background: ${props.background};`}
-  ${props =>
+	${props => props.height && `height: ${props.height};`}
+	${props => props.width && `width: ${props.width};`}
+	${props => props.minWidth && `min-width: ${props.minWidth + 'px'};`}
+	${props => props.maxWidth && `max-width: ${props.maxWidth + 'px'};`}
+	${props => props.background && `background: ${props.background};`}
+	${props =>
 		props.fill &&
 		`
-    width: 100%;
-    height: 100%;
-    min-width: 100%;
-    min-height: 100%;
-  `}
+			width: 100%;
+			height: 100%;
+			min-width: 100%;
+			min-height: 100%;
+		`}
 `;
 
 export const Flex = styled(FlexItem)`
