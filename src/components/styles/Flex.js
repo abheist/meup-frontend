33a12cda4770b78a08860805fcfa0a8fbe2568b3
abheist <abhishek291093@gmail.com>
2020-03-25 15:59:31 +0530

    refactoring | changes flexes to flex and bit of proptypes validations

---
+++ src/components/styles/Flex.js
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { SPACINGS } from './ThemeConstants';
+
+export const Flex = styled.div`
+  display: flex;
+  ${props => props.directions && `flex-direction: ${props.direction};`}
+  ${props => props.justify && `justify-content: ${props.justify};`}
+  ${props => props.content && `align-content: ${props.content};`}
+  ${props => props.align && `align-items: ${props.align};`}
+  ${props => props.wrap && `flex-wrap: ${props.wrap};`}
+  ${props => props.flex && `flex: ${props.flex};`}
+  ${props => props.order && `order: ${props.order};`}
+  ${props => props.self && `align-self: ${props.self};`}
+  ${props => props.margin && `margin: ${props.margin + 'px'};`}
+  ${props => props.padding && `padding: ${props.padding + 'px'};`}
+`;
+
+Flex.propTypes = {
+  direction: PropTypes.oneOf([
+    'row',
+    'row-reverse',
+    'column',
+    'column-reverse'
+  ]),
+  justify: PropTypes.oneOf([
+    'flex-start',
+    'flex-end',
+    'center',
+    'space-between',
+    'space-around ',
+    'space-evenly'
+  ]),
+  content: PropTypes.oneOf([
+    'flex-start',
+    'flex-end',
+    'center',
+    'space-between',
+    'space-around',
+    'space-evenly',
+    'stretch',
+    'baseline'
+  ]),
+  align: PropTypes.oneOf([
+    'stretch',
+    'flex-start',
+    'flex-end',
+    'center',
+    'baseline'
+  ]),
+  wrap: PropTypes.oneOf(['nowrap', 'wrap', 'wrap-reverse']),
+  flex: PropTypes.oneOf([
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12'
+  ]),
+  order: PropTypes.oneOf([
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12'
+  ]),
+  self: PropTypes.oneOf([
+    'auto',
+    'flex-start',
+    'flex-end',
+    'center',
+    'baseline',
+    'stretch'
+  ]),
+  margin: SPACINGS,
+  padding: SPACINGS
+};
