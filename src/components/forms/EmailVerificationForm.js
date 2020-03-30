9e4d611b58a5962c0e3a48d206756beca60ec859
abheist <abhishek291093@gmail.com>
2020-03-30 15:10:47 +0530

    :tada: Completed the signup basic flow with email verification

--- src/components/forms/EmailVerificationForm.js
+++ src/components/forms/EmailVerificationForm.js
@@ -1,35 +1,31 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Redirect } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { QL_MUTATION_VERIFY_ACCOUNT_TOKEN } from '../../graphql/mutations/authentication';
 import { toast } from 'react-toastify';
 
 function EmailVerificationForm() {
-	const [doVerifyToken, { data, loading, error }] = useMutation(
+	const [doVerifyToken, { data }] = useMutation(
 		QL_MUTATION_VERIFY_ACCOUNT_TOKEN
 	);
 
 	let { token } = useParams();
 
 	useEffect(() => {
-		console.log('calling');
 		doVerifyToken({ variables: { token: token } });
 	}, [token, doVerifyToken]);
 
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error :(</p>;
-
 	if (data?.verifyAccount?.success) {
-		return <div>verified</div>;
+		toast('🎉 Account verified!!!');
+		return <Redirect to="/" />;
 	} else if (
 		data?.verifyAccount?.errors?.nonFieldErrors[0].code ===
 		'already_verified'
 	) {
 		toast('Account already verified!!!');
-		console.log('Account already verified!!!');
-		return <div>Account already verified!!!</div>;
+		return <Redirect to="/" />;
 	}
-	return <div>Something!!!</div>;
+	return <div>There is some problem. Please contact: hi@zucked.club</div>;
 }
 
 export default EmailVerificationForm;
