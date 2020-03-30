42767bad8d87b973d83f6d1b72d62df0b94890f6
abheist <abhishek291093@gmail.com>
2020-03-30 14:58:12 +0530

    :construction: added basic toast

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -12,6 +12,8 @@ import AppHandler from '../appHandler/AppHandler';
 import { setContext } from '@apollo/link-context';
 import { AUTH_TOKEN_NAME } from '../../helpers/authService';
 import { BrowserRouter } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.min.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
 	const httpLink = new HttpLink({ uri: 'http://localhost:8000/graphql/' });
@@ -35,6 +37,7 @@ function App() {
 		<BrowserRouter>
 			<ApolloProvider client={client}>
 				<Theme>
+					<ToastContainer />
 					<AppHandler />
 				</Theme>
 			</ApolloProvider>
--- src/components/forms/EmailVerificationForm.js
+++ src/components/forms/EmailVerificationForm.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { QL_MUTATION_VERIFY_ACCOUNT_TOKEN } from '../../graphql/mutations/authentication';
+import { toast } from 'react-toastify';
 
 function EmailVerificationForm() {
 	const [doVerifyToken, { data, loading, error }] = useMutation(
@@ -24,6 +25,7 @@ function EmailVerificationForm() {
 		data?.verifyAccount?.errors?.nonFieldErrors[0].code ===
 		'already_verified'
 	) {
+		toast('Account already verified!!!');
 		console.log('Account already verified!!!');
 		return <div>Account already verified!!!</div>;
 	}
