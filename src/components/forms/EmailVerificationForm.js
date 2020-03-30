d8beae6780d582fbee8c9c9b28866ae66f28beee
abheist <abhishek291093@gmail.com>
2020-03-30 14:21:46 +0530

    :construction: added basic emai verification layout

--- src/components/forms/EmailVerificationForm.js
+++ src/components/forms/EmailVerificationForm.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { QL_MUTATION_VERIFY_ACCOUNT_TOKEN } from '../../graphql/mutations/authentication';
 
-function EmailVerificationForm(props) {
-	return <div>Hello</div>;
+function EmailVerificationForm() {
+	const [doVerifyToken, { data, loading, error }] = useMutation(
+		QL_MUTATION_VERIFY_ACCOUNT_TOKEN
+	);
+
+	let { token } = useParams();
+
+	useEffect(() => {
+		console.log('calling');
+		doVerifyToken({ variables: { token: token } });
+	}, [token, doVerifyToken]);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :(</p>;
+
+	if (data?.verifyAccount?.success) {
+		return <div>verified</div>;
+	} else if (
+		data?.verifyAccount?.errors?.nonFieldErrors[0].code ===
+		'already_verified'
+	) {
+		console.log('Account already verified!!!');
+		return <div>Account already verified!!!</div>;
+	}
+	return <div>Something!!!</div>;
 }
 
 export default EmailVerificationForm;
